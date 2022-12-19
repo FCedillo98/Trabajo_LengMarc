26a74628711e04df44e187a4471e2fb583689783
FCedillo98 <118803632+FCedillo98@users.noreply.github.com>
2022-12-19 10:45:35 +0100

    Primera subida
    Primera versión con algunas fotos faltantes.

---
+++ js/trabajo.js
@@ -0,0 +1,63 @@
+/*************************************************************************************************/
+/*  Web del Romanticismo   - dunciones de js para snippets                                       */
+/*  CSS de Autores románticos                                                                    */
+/*  Autor: Francisco Cedillo Serrato                                                             */
+/*  Versión 1.0                                                                                  */
+/*************************************************************************************************/
+
+/*********************** BARRA DE VANVEGACIÓN *********************************/
+
+
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myTopnav").style.top = "0";
+  } else {
+    document.getElementById("myTopnav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  } 
+
+/*********************** FUNCIÓN PARA HERO SECTION ****************************/ 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
